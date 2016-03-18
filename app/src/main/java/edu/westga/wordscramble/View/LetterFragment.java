15f1eb1a1d0ecfb7d6d16356454f1c6ba1f35829
Cory Alford <calford4@my.westga.edu>
2016-03-17 22:37:34 -0700

    Added private method to MainActivity to create LetterFragments with the appropriate letter. Fixed sizing and other issues to display LetterFragments properly.

--- app/src/main/java/edu/westga/wordscramble/View/LetterFragment.java
+++ app/src/main/java/edu/westga/wordscramble/View/LetterFragment.java
@@ -1,11 +1,14 @@
 package edu.westga.wordscramble.View;
 
 
+import android.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 import edu.westga.wordscramble.R;
 
@@ -14,16 +17,17 @@ import edu.westga.wordscramble.R;
  */
 public class LetterFragment extends Fragment {
 
-
     public LetterFragment() {
         // Required empty public constructor
     }
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_letter, container, false);
-    }
+        View view = inflater.inflate(R.layout.fragment_letter, container, false);
 
+        TextView textView = (TextView) view.findViewById(R.id.fragmentLetterTextView);
+        textView.setText(this.getArguments().getString("KEY"));
+        return view;
+    }
 }
--- app/src/main/java/edu/westga/wordscramble/View/MainActivity.java
+++ app/src/main/java/edu/westga/wordscramble/View/MainActivity.java
@@ -1,8 +1,10 @@
 package edu.westga.wordscramble.View;
 
+
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -14,6 +16,7 @@ import edu.westga.wordscramble.R;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String KEY = "KEY";
     private WordController controller;
 
     @Override
@@ -21,27 +24,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.controller = new WordController();
+
+        FragmentManager fm = this.getFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        LetterFragment frag = newLetterFragment("M");
+        ft.add(R.id.fragment_container, frag);
+
+        LetterFragment frag2 = newLetterFragment("L");
+        ft.add(R.id.fragment_container, frag2);
+
+        LetterFragment frag3 = newLetterFragment("I");
+        ft.add(R.id.fragment_container, frag3);
+
+        LetterFragment frag4 = newLetterFragment("K");
+        ft.add(R.id.fragment_container, frag4);
+
+        LetterFragment frag5 = newLetterFragment("J");
+        ft.add(R.id.fragment_container, frag5);
+
+        LetterFragment frag6 = newLetterFragment("L");
+        ft.add(R.id.fragment_container, frag6);
+
+        ft.commit();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+    /**
+     * Create a new instance of LetterFragment, initialized to
+     * show the text at 'index'.
+     */
+    private static LetterFragment newLetterFragment(String letter) {
+        LetterFragment f = new LetterFragment();
+        // Supply index input as an argument.
+        Bundle args = new Bundle();
+        args.putString(KEY, letter);
+        f.setArguments(args);
+        return f;
     }
 }
