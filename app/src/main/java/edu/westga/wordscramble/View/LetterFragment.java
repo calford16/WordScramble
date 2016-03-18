f82c181fdcb9104c8253b8e6245d9c843d179f6e
Cory Alford <calford4@my.westga.edu>
2016-03-17 21:28:29 -0700

    Added LetterFragment fragment and created an XML layout for the fragment

---
+++ app/src/main/java/edu/westga/wordscramble/View/LetterFragment.java
@@ -0,0 +1,29 @@
+package edu.westga.wordscramble.View;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import edu.westga.wordscramble.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LetterFragment extends Fragment {
+
+
+    public LetterFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_letter, container, false);
+    }
+
+}
